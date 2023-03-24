87afeb85ebf9f9b4cd3b41ff91cb4bb549cd5cc7
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2023-03-24 11:52:17 +0100

    kotlin basics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val shoppingList = mutableListOf("eggs", "shoes", "books")
+    shoppingList.add(2,"welcome")
+    println(shoppingList[3])
+//    val number1 = readlnOrNull() ?: "0"
+//    val number2 = readlnOrNull() ?: "0"
+//    val result = number1.toInt() * number2.toInt()
+//    val amIAnAdult = true
+//    val amIAProgrammer = true
+//    val amIAnAdultProgrammer = amIAnAdult && amIAProgrammer
+//    val amIAnAdultProgrammer2 = (amIAnAdult == amIAProgrammer)
+//    val x = 9234
+//    val mainInteger = 3.234f
+//    val y: Boolean = true
+//    val string : String = "Kotlin is actually not bad"
+//    println(string.uppercase())
+//    println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+//    println("the value of mainInteger is: $mainInteger")
+//    println("the value of x is: $x")
+//    println("the value of y is: $y")
+//    println("am I an adult Programmer: $amIAnAdultProgrammer")
+//    println("am I an adult Programmer second iteration: $amIAnAdultProgrammer2")
 }
